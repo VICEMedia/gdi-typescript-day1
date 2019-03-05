3c0658a26f5629f2d36f889b1db1d55a059aab0f
Paul Fielek <Paul Fielek>
2019-03-05 18:03:08 -0500

    Added some more slides and examples

---
+++ presentation/snippets/syntax-example.ts
@@ -0,0 +1,4 @@
+const myString: string = 'Hello';
+function sayHello(name: string): string {
+  return `${myString} ${name}`;
+}
--- presentation/theme.js
+++ presentation/theme.js
@@ -1,10 +1,16 @@
 import theme from 'mdx-deck/themes';
-import okaidia from 'react-syntax-highlighter/styles/prism/okaidia';
+import okaidia from 'react-syntax-highlighter/styles/prism/prism';
+import prismJSON from 'react-syntax-highlighter/languages/prism/json';
+import prismTypescript from 'react-syntax-highlighter/languages/prism/typescript';
 
 export default {
   ...theme,
   prism: {
-    style: okaidia
+    style: okaidia,
+    languages: {
+      json: prismJSON,
+      typescript: prismTypescript,
+    }
   },
   h1: {
     'border-bottom': 'black solid 5px',
